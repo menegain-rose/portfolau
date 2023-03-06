1175011db856b61b9724cafd8b521b02c3f2e37b
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2023-03-06 12:46:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+
+<header>
+
+</header>
+
+<main>
+  <RouterView />
+</main>
+
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      isDark: true,
+    };
+  },
+  components: {
+    
+  },
+};
+</script>
