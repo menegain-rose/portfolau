983780f780281aa2ff879eacbfdf7b39d574c3d3
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2023-03-09 18:58:23 +0100

    Avancement page d'accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,43 +2,50 @@
 </script>
 
 <template>
-    <div class="bg-darkgreen flex flex-col items-center justify-center h-screen">
+    <div class="bg-darkgreen flex flex-col items-center justify-start min-h-screen">
         <div class="bg-cleargreen pt-40 px-20 rounded-t-full">
             <H1 class="text-whiteb">Rose MENEGAIN</H1>
-            <img src="/public/images/Portrait.png" class="w-96 h-auto" alt="">
+            <img src="/public/images/Portrait.png" class="w-72 sm:w-96 h-auto" alt="">
         </div>
     </div>
 
-    <section class="my-20 grid grid-cols-2">
-        <div class="bg-clearbeige py-10 px-20">
+    <section class="my-10 sm:my-20 grid grid-cols-1 sm:grid-cols-2">
+        <div class="bg-clearbeige px-8 py-14 sm:p-20">
             <h2>Mes travaux</h2>
-            <p class="pt-5">
-                Découvrez ici mes divers projets, ainsi que mes réalisations dans les domaines du design et du développement web. <br>
-                Je suis heureuse de vous présenter mes compétences dans ces différents domaines et j'espère que vous apprécierez mes différents travaux. <br>
-                En parcourant mes projets vous allez pouvoir comprendre mes compétences en matière de design et de développement. <br>
-                Si vous souhaitez discuter de projets futurs et me poser des questions, je vous invite à me contacter sur mes différents réseaux ou par mail..
-
-                https://sae203.rmenegain.fr/ <br>
-                https://rearts.leopold-ohnimus.fr/ <br>
-                https://dazzling-lebkuchen-42612d.netlify.app <br>
-
-                https://www.behance.net/gallery/160577691/MMI-InternationalMobility
+            <p class="pt-10">
+                Découvrez ici mes divers projets, ainsi que mes réalisations dans les domaines du design et du développement web.
             </p>
+            <p class="pt-3">
+                Je suis heureuse de vous présenter mes compétences dans ces différents domaines et j'espère que vous apprécierez mes différents travaux. <br>
+            </p>
+            <p class="pt-3">
+                En parcourant mes projets vous allez pouvoir comprendre mes compétences en matière de design et de développement.
+            </p>
+            <p class="pt-3">
+                Si vous souhaitez discuter de projets futurs et me poser des questions, je vous invite à me contacter sur mes différents réseaux ou par mail..
+            </p>
+            <div class="flex mt-2 sm:mt-5">
+                <img src="/public/Icons/Discord.svg" class="w-8 mr-5" alt="">
+                <img src="/public/Icons/Instagram.svg" class="w-8 mr-5" alt="">
+                <img src="/public/Icons/Linkedin.svg" class="w-8 mr-5" alt="">
+            </div>
         </div>
-        <div class="mx-10">
-            <div class="flex items-center bg-brown my-10 pt-5 pb-3 px-5 rounded-lg">
-                <img src="/public/images/Naknowledge.png" class="w-auto h-60" alt="">
-                <h2 class="text-whiteb">Projets</h2>
+
+        <div class="mx-2 sm:mx-10">
+            <div class="flex flex-col sm:flex-row items-center bg-brown my-5 py-5 px-5 rounded-lg">
+                <img src="/public/images/Naknowledge.png" class="w-auto h-60 order-last sm:order-first" alt="">
+                <h2 class="text-whiteb mb-5 sm:ml-10">Projets</h2>
             </div>
-            <div class="flex items-center bg-brown my-10 p-5 rounded-lg">
-                <img src="/public/images/Design/Cassandre_RoseMENEGAIN_A2.svg" class="w-auto h-60" alt="">
-                <h2 class="text-whiteb pl-20">Designs</h2>
+            <div class="flex flex-col sm:flex-row items-center bg-brown my-5 py-5 px-5 rounded-lg">
+                <img src="/public/images/Design/Cassandre_RoseMENEGAIN_A2.svg" class="w-auto h-60 order-last sm:order-first" alt="">
+                <h2 class="text-whiteb mb-5 sm:ml-10">Designs</h2>
             </div>
-            <div class="flex items-center bg-brown my-10 p-5 rounded-lg">
-                <img src="/public/images/sae301.png" class="w-auto h-60" alt="">
-                <h2 class="text-whiteb pl-20">Développement</h2>
+            <div class="flex flex-col sm:flex-row items-center bg-brown my-5 py-5 px-5 rounded-lg">
+                <img src="/public/images/sae301.png" class="w-auto h-60 order-last sm:order-first" alt="">
+                <h2 class="text-whiteb mb-5 sm:ml-10">Développement</h2>
             </div>
         </div>
+
     </section>
 
     <section>
