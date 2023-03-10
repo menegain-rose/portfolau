1739731ba768c5d23f2b4da030c46c17771cceed
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2023-03-10 22:17:03 +0100

    création base page design

--- src/views/DesignView.vue
+++ src/views/DesignView.vue
@@ -5,40 +5,71 @@ import Bouton from "@/components/bouton.vue"
 
 <template>
 
-<section class="my-10 sm:my-20 grid grid-cols-1 sm:grid-cols-2">
-        <div class="bg-clearbrown px-8 py-14 sm:p-20 grid grid-cols-2">
-            <div>
-                <h2>Designs</h2>
-    
-                <h3 class="pt-5">Maquettes</h3>
-                <p class="pt-2">
-                    J'ai pu confectionner et imaginer différentes maquettes depuis le début de mes études en BUT MMI. <br>
-                    Mes différentes maquettes vous offres des exemples de mon expérience en matière de conception d'interfaces utilisateur (UI) et d'expérience utilisateur (UX), <br>
-                    Sur lequel j'ai pu m'adapter aux demandes des divers commanditaires et donc améliorer mes designs pour répondre aux demandes et surtout aux besoins des futures utilisateurs.
-                </p>
-    
-                <h3 class="pt-10">Affiches</h3>
-                <p class="pt-2">
-                    J'ai également pu faire quelques affiches et cartes postales, que ce sois pour méxercer, ou lier avec le BUT. <br>
-                    Ces projets m'ont aider à approfondir ma créativité et mes compétences en design graphiques.
-                </p>
-    
-                <h3 class="pt-10">Logos</h3>
-                <p class="pt-2">
-                    J'ai également pu faire quelques affiches et cartes postales, que ce sois pour méxercer, ou lier avec le BUT. <br>
-                    Ces projets m'ont aider à approfondir ma créativité et mes compétences en design graphiques.
-                </p>
-    
-                <div class="text-center">
-                    <Bouton class="mt-10">
-                        <router-link to="/Design">
-                            Voir plus
-                        </router-link>
-                    </Bouton>
+    <section>
+        <h2>Designs</h2>
+         <div>
+            <h3>
+                Maquettes
+            </h3>
+            <div class="grid grid-cols-2">
+                <div>
+                    <a href="">
+                        <img src="" alt="">
+                    </a>
                 </div>
-            </div>
+                <div>
+                    <h4>
+                        Naknowledge
+                    </h4>
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus eligendi reprehenderit ipsum aspernatur corporis magni ullam dolores corrupti voluptatum. Amet expedita assumenda veniam incidunt dolorem, quo exercitationem architecto maiores commodi!
+                    </p>
+                    <h5>
+                        Acteurs
+                    </h5>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                    </p>
+                    <h5>
+                        Dates
+                    </h5>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                    </p>
+                    <h5>
+                        Objectifs
+                    </h5>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                    </p>
+                    <h5>
+                        Outils
+                    </h5>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                    </p>
+                    <h5>
+                        Mon rôle
+                    </h5>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                    </p>
+                </div>
 
-        </div>
+            </div>
+            <div>
+                <img src="" alt="">
+                <legend></legend>
+                <img src="" alt="">
+                <legend></legend>
+                <img src="" alt="">
+                <legend></legend>
+            </div>
+         </div>
+
+
     </section>
+
+
 
 </template>
