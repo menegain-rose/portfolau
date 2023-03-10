5e999ccace1c76e41fb313fef8d353439e2a359b
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2023-03-11 00:08:23 +0100

    Première partie page design pratiquement fini

--- src/views/DesignView.vue
+++ src/views/DesignView.vue
@@ -5,68 +5,112 @@ import Bouton from "@/components/bouton.vue"
 
 <template>
 
-    <section>
-        <h2>Designs</h2>
-         <div>
-            <h3>
-                Maquettes
-            </h3>
-            <div class="grid grid-cols-2">
-                <div>
-                    <a href="">
-                        <img src="" alt="">
+<section class="pt-8 sm:pt-20">
+    <div class="flex justify-between">
+        <h2 class="px-4 sm:px-20 text-center sm:text-left">Designs</h2>
+        <img src="/public/images/Design/PlantesTombantesLongues.svg" class="rotate-90 w-1/5" alt="">
+    </div>
+    <div>
+        <h3 class="my-6 sm:my-10 mx-4 sm:mx-20 text-center sm:text-left">
+            Maquettes
+        </h3>
+        <div class="sm:grid sm:grid-cols-2">
+            <div class="px-4 sm:px-20">
+                <div class="flex justify-center items-center flex-col mt-6 sm:mt-10">
+                    <a href="https://www.figma.com/proto/wd8j2PMyyL3Po6uiISYypJ/maquette-naknowledge?node-id=932%3A3515&scaling=scale-down&page-id=1%3A4&starting-point-node-id=932%3A3515&show-proto-sidebar=1">
+                        <img src="/public/images/Design/Naknowledge02.png" class="h-64 sm:h-96 w-auto" alt="">
                     </a>
                 </div>
-                <div>
-                    <h4>
+                <div class="flex justify-center items-center flex-col mt-6 sm:mt-10">
+                    <img src="/public/images/Design/MoodboardNkwg.png" class="h-auto w-2/3 sm:w-auto rounded-lg" alt="">
+                    <img src="/public/logo/naknowledge.png" class="h-auto w-2/3 sm:w-auto rounded-lg mt-6 sm:mt-10" alt="">
+                </div>
+                <div class="text-center mt-6 sm:mt-10">
+                    <Bouton class="my-4 sm:my-10">
+                        <router-link to="/Design">
+                            Découvrir
+                        </router-link>
+                    </Bouton>
+                </div>
+            </div>
+                <div class="bg-clearbeige p-20">
+                    <h4 class="mb-5">
                         Naknowledge
                     </h4>
                     <p>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus eligendi reprehenderit ipsum aspernatur corporis magni ullam dolores corrupti voluptatum. Amet expedita assumenda veniam incidunt dolorem, quo exercitationem architecto maiores commodi!
+                        Naknowledge est un site web destiné à l'apprentissage gratuit et ludique pour les étudiants. <br> 
+                        Elle propose diverse nano-leçons sur un ensemble de sujets variés. <br> 
+                        Ces leçons sont consultables à n'importe quel moment et peuvent être créé par l'utilisateur et déposer sur le site. <br>
+                        Ces nano-leçons ont pour vocations de présenter un savoir/savoir-faire qui peuvent être obtenu et assimilé en quelques minutes, <br>
+                        d'où le mot “nano” signifiant “petit”. <br>
+                        Étant donné que Naknowledge propose du contenu ludique, <br> 
+                        les nano leçons sontaccompagnées de liens vers d'autres nano leçons corrélées et similaires aux leçons précédemment étudiés.
                     </p>
-                    <h5>
+                    <h5 class="mt-5">
                         Acteurs
                     </h5>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
-                    </p>
-                    <h5>
+                    <div class="flex">
+                        <p class="underline mr-2">
+                            Commanditaires :
+                        </p>
+                        <p>
+                            Pascal Chatonnay
+                        </p>
+                    </div>
+                    <div class="flex">
+                        <p class="underline mr-2">
+                        Tuteur :
+                        </p>
+                        <p>
+                            Lydia Dubois
+                        </p>
+                    </div>
+                    <div class="flex">
+                        <p class="underline mr-2">
+                        Prestataires :
+                        </p>
+                        <p>
+                            Mélina BOUCHELLEGHEM, David SARKISSIAN, <br>
+                            Aurélie RUNSER, et moi même.
+                        </p>
+                    </div>
+
+                    <h5 class="mt-5">
                         Dates
                     </h5>
                     <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                        06/09/2022 → 17/10/2022 → 17/01/2023
                     </p>
-                    <h5>
+                    <h5 class="mt-5">
                         Objectifs
                     </h5>
                     <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                        - Créer un site web ergonomique et attractif pour l'apprentissage et le partage simple de nano-leçons . <br>
+                        - Complètement opérationnelle. <br>
+                        - Obtenir une vingtaine d'abonnement sur chaque réseau social. <br>
+                        - Quelques réactions des personnes sur les réseaux sociaux (like/ partage/commentaires). <br>
+                        - Proposer une cinquantaine (au moins) de nano-leçon au lancement du site. <br>
+                        - Rendre le site “autonome” en contenus, alimenté par les utilisateurs eux-mêmes.
                     </p>
-                    <h5>
+                    <h5 class="mt-5">
                         Outils
                     </h5>
                     <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                        HTML5, CSS3, Tailwind, Javascript, et PHP. <br>
+                        Figma, Première Pro, et Illustrator. <br>
+                        Visual Studio Code, Github, FileZilla, Wordpress, et Infomaniak.
+
                     </p>
-                    <h5>
+                    <h5 class="mt-5">
                         Mon rôle
                     </h5>
                     <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut fuga ipsum recusandae nobis vitae enim impedit? Eligendi officia impedit molestiae laboriosam vitae voluptates minima repellat assumenda voluptate nostrum, in quidem?
+                        Mon rôle a été de créer l'aspect visuel du site (logo, intentions graphiques, harmonie colorée..). <br> 
+                        J'ai dû prêter attention à la maquette graphique en la rendant intuitive et ergonomique pour l'utilisateur.
                     </p>
                 </div>
-
-            </div>
-            <div>
-                <img src="" alt="">
-                <legend></legend>
-                <img src="" alt="">
-                <legend></legend>
-                <img src="" alt="">
-                <legend></legend>
             </div>
          </div>
-
 
     </section>
 
