c4d565dc175cf51a7acb7efa070118129a05ef8e
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2023-03-12 22:59:47 +0100

    Retouche page 404

--- src/views/PageNotFoundView.vue
+++ src/views/PageNotFoundView.vue
@@ -1,5 +1,4 @@
 <template>
-
     <div class="items-center text-center p-5 py-52">
         <p class="m-5">
             Page non trouvée. La page que vous avez demandée n'a pas été trouvée
@@ -8,29 +7,25 @@
             Page not found. The page you requested was not found
         </p>
         <router-link to="/">
-        <Bouton type="Bouton" @click="redirectMe()" class="" > 
-            Retour à l'accueil 
-        </Bouton>
+            <Bouton type="Bouton" @click="redirectMe()" class=""> 
+                Retour à l'accueil 
+            </Bouton>
         </router-link>
-
     </div>
-
 </template>
-
-
-
 
 <script>
 import Bouton from '/src/components/bouton.vue'
+
 export default {
-        name: "PageNotFound",
-        methods: {
-            redirectMe(){
-                this.$router.push({ name: 'Accueil'})
-            }
-        },
-        components: { 
-            Bouton,
+    name: "PageNotFound",
+    methods: {
+        redirectMe(){
+            this.$router.push({ name: 'Accueil'})
         }
+    },
+    components: { 
+        Bouton,
+    }
 }
 </script>
