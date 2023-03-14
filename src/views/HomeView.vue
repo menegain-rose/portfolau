fbcec7bf7b79f677e10ea80caff1aa91ca0e7bc1
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2023-03-14 17:56:40 +0100

    correction erreur scrool → animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,9 @@ import Bouton from "@/components/bouton.vue"
 
 window.addEventListener('scroll', function() {
     var animation = document.querySelector('.animation');
+    if (animation === null) {
+        return
+    }
     var position = animation.getBoundingClientRect().top;
     var screen = window.innerHeight;
     if (position < screen) {
@@ -19,7 +22,11 @@ window.addEventListener('scroll', function() {
             <img src="/public/images/Portrait.png" class="w-72 sm:w-96 h-auto" alt="Portrait dessin Rose MENEGAIN">
         </div>
         <div class="animation">
-            <svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" viewBox="0 0 1024 1024"><path fill="#F5F2EC" d="M831.872 340.864L512 652.672L192.128 340.864a30.592 30.592 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.592 30.592 0 0 0-42.752 0z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" 
+                width="5em" 
+                height="5em" 
+                viewBox="0 0 1024 1024">
+                <path fill="#F5F2EC" d="M831.872 340.864L512 652.672L192.128 340.864a30.592 30.592 0 0 0-42.752 0a29.12 29.12 0 0 0 0 41.6L489.664 714.24a32 32 0 0 0 44.672 0l340.288-331.712a29.12 29.12 0 0 0 0-41.728a30.592 30.592 0 0 0-42.752 0z"></path></svg>
         </div>
         
     </div>
